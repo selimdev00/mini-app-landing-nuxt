59fc4b8b3cb93d06500f21d4ff1d4b8347e8d824
Selim Ataballyev <selimdev00@gmail.com>
2025-09-09 06:08:19 +0500

    [init]

---
+++ app/components/Header.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const links = [
+  {
+    title: "Возможности",
+  },
+  {
+    title: "Для кого",
+  },
+  {
+    title: "Как работает",
+  },
+  {
+    title: "Подписка",
+  },
+  {
+    title: "Вопросы",
+  },
+  {
+    title: "Поддержка",
+  },
+];
+</script>
+
+<template>
+  <header>
+    <ul>
+      <li v-for="(item, index) in links" :key="item.title">
+        {{ item.title }}
+      </li>
+    </ul>
+  </header>
+</template>
