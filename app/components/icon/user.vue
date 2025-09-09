e5854bae173da37187cce147db52df2f0e08a964
Selim Ataballyev <selimdev00@gmail.com>
2025-09-09 08:37:15 +0500

    [landing]: catalog section desktop

---
+++ app/components/icon/user.vue
@@ -0,0 +1,71 @@
+<template>
+  <svg
+    width="29"
+    height="29"
+    viewBox="0 0 29 29"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_2584_9764)">
+      <path
+        d="M22.5669 3.90039H6.2336C4.94494 3.90039 3.90027 4.94506 3.90027 6.23372V22.5671C3.90027 23.8557 4.94494 24.9004 6.2336 24.9004H22.5669C23.8556 24.9004 24.9003 23.8557 24.9003 22.5671V6.23372C24.9003 4.94506 23.8556 3.90039 22.5669 3.90039Z"
+        fill="url(#paint0_linear_2584_9764)"
+        stroke="url(#paint1_linear_2584_9764)"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.4003 15.5674C16.3333 15.5674 17.9003 14.0004 17.9003 12.0674C17.9003 10.1344 16.3333 8.56738 14.4003 8.56738C12.4673 8.56738 10.9003 10.1344 10.9003 12.0674C10.9003 14.0004 12.4673 15.5674 14.4003 15.5674Z"
+        fill="#252525"
+        stroke="#252525"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8.56702 27.2334V23.7334C8.56702 22.8051 8.81285 21.9149 9.25043 21.2585C9.68802 20.6021 10.2815 20.2334 10.9004 20.2334H17.9004C18.5192 20.2334 19.1127 20.6021 19.5503 21.2585C19.9879 21.9149 20.2337 22.8051 20.2337 23.7334V27.2334"
+        fill="#252525"
+      />
+      <path
+        d="M8.56702 27.2334V23.7334C8.56702 22.8051 8.81285 21.9149 9.25043 21.2585C9.68802 20.6021 10.2815 20.2334 10.9004 20.2334H17.9004C18.5192 20.2334 19.1127 20.6021 19.5503 21.2585C19.9879 21.9149 20.2337 22.8051 20.2337 23.7334V27.2334"
+        stroke="#252525"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <linearGradient
+        id="paint0_linear_2584_9764"
+        x1="3.90027"
+        y1="14.4004"
+        x2="24.9003"
+        y2="14.4004"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#F63450" />
+        <stop offset="1" stop-color="#D22737" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_2584_9764"
+        x1="3.90027"
+        y1="14.4004"
+        x2="24.9003"
+        y2="14.4004"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#F63450" />
+        <stop offset="1" stop-color="#D22737" />
+      </linearGradient>
+      <clipPath id="clip0_2584_9764">
+        <rect
+          width="28"
+          height="28"
+          fill="white"
+          transform="translate(0.400269 0.400391)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
