9be25da69041aa67bce02eed2ae461fe6cc09104
Selim Ataballyev <selimdev00@gmail.com>
2025-09-17 20:30:23 +0500

    [seo]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,29 @@ export default defineNuxtConfig({
     prefix: "",
     componentDir: "./components/ui",
   },
+  app: {
+    head: {
+      title: "Марскакод - Мini App в Телеграм",
+      htmlAttrs: {
+        lang: "ru",
+      },
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon-96x96.png",
+          sizes: "96x96",
+        },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "shortcut icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
 });
